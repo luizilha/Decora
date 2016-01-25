979c3ff83e61efa32cf3df2f60e03dcca131db45
LUIZ ILHA <ilha@LUIZs-MacBook-Pro.local>
2016-01-25 16:34:47 -0200

    categoria pk

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -1,5 +1,6 @@
 class Categoria < ActiveRecord::Base
   self.table_name = 'categoria'
+  self.primary_key = 'id_categoria'
 
   has_attached_file :capa, styles: { original: "500x500>", medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :capa, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
