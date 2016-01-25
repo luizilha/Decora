bbad08a8fe9978619766c17a3ec9f7ec834e4e96
LUIZ ILHA <ilha@LUIZs-MacBook-Pro.local>
2016-01-25 14:25:12 -0200

    testando deletar

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -1,7 +1,7 @@
 class Categoria < ActiveRecord::Base
   self.table_name = 'categoria'
 
-  has_attached_file :capa, styles: { original: "500x500>", medium: "300x300>", thumb: "100x100>" }, convert_options: {:thumb => '-resize 100x100'}
+  has_attached_file :capa, styles: { original: "500x500>", medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :capa, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates_presence_of :nome, message: "deve preencher campo nome"
   belongs_to :admin
