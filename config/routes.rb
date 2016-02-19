c0216c919040dec22d9211297c48661439658e7f
LUIZ ILHA <ilha@LUIZs-MacBook-Pro.local>
2016-02-19 12:06:53 -0200

    mudanca de layout e escolher categoria no item funfando

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   match 'itens' => 'item#lista', via: 'get'
   match 'itens/:id' => 'item#listaDaCategoria', via: 'get'
   match 'item/novo/:id' => 'item#novo', via: 'get'
+  match 'item/novo' => 'item#novo', via: 'get'
   match 'itens/:id' => 'item#cria', via: 'post', path: 'cria'
   match 'item/deleta/:id/:id_categoria' => 'item#deleta', via: 'get'
   match 'item/edita/:id' => 'item#edita', via: 'get'
