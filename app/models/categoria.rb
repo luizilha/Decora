c88cc46a05f9cb8c5994af167c2e4f52d2a4da6e
LUIZ ILHA <ilha@LUIZs-MacBook-Pro.local>
2016-03-15 00:11:59 -0300

    teste com a pasta da categoria

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -4,7 +4,7 @@ class Categoria < ActiveRecord::Base
 	belongs_to :admin
 	has_many :item, foreign_key: 'id_categoria', dependent: :destroy
 	#Validacoes
-	has_attached_file :capa, styles: { original: "500x500>" }, url: '/assets/:id.:extension', path: ':rails_root/public/assets/:id.:extension'
+	has_attached_file :capa, styles: { original: "500x500>" }, url: '${OPENSHIFT_DATA_DIR}/assets/:id.:extension', path: ':rails_root/public/assets/:id.:extension'
 	validates_attachment_content_type :capa, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"], message: 'Arquivo deve ser imagem!'
 	validates_presence_of :nome, message: "deve preencher campo nome"
 	validates_presence_of :capa, message: "deve conter uma imagem"
