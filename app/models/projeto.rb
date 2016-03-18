964c023f7b26f0dfad8ffdfb5821676d0c341135
LUIZ ILHA <ilha@LUIZs-MacBook-Pro.local>
2016-03-18 00:17:40 -0300

    mudanca de diretorio de projeto categoria e item funcionando correto

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -2,7 +2,7 @@ class Projeto < ActiveRecord::Base
   self.table_name = 'projeto'
   has_and_belongs_to_many :item, join_table: 'item_projeto', foreign_key: 'id_projeto', association_foreign_key: 'id_item'
 
-  has_attached_file :capa, styles: { original: "500x500>" }
+  has_attached_file :capa, styles: { original: "500x500>" }, url: '/uploads/:class/:attachment/:id/:style_:filename', path: ':rails_root/public/uploads/:class/:attachment/:id/:style_:filename'
   validates_attachment_content_type :capa, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"], message: 'Arquivo deve ser imagem!'
   validates_presence_of :nome, message: 'deve ser preenchido'
 end
