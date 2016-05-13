8301ee98f27492190853b88710b0656e70544434
LUIZ ILHA <ilha@LUIZs-MacBook-Pro.local>
2016-05-13 08:43:41 -0300

    teste de email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  # fiz de tudo
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
